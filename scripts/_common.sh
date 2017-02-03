d0da2c78de394c1541076f385aa269ef98feec6b
Julien Malik <julien.malik@paraiso.me>
2017-02-03 16:51:25 +0100

    [enh] Update upstream to 1.2.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,13 +3,13 @@
 #
 
 # Roundcube version
-VERSION="1.2.2"
+VERSION="1.2.3"
 
 # Package name for Roundcube dependencies
 DEPS_PKG_NAME="roundcube-deps"
 
 # Roundcube complete tarball checksum
-ROUNDCUBE_SOURCE_SHA256="6c647ed28ea0ae1b730a1784018b63884e65ed6036208e9c3a6ee62123fe856d"
+ROUNDCUBE_SOURCE_SHA256="2df820d2ccc7bb320f854a821a1dc9983792f42a3353a1d38fe0822d94980d4d"
 
 # Remote URL to fetch Roundcube source tarball
 ROUNDCUBE_SOURCE_URL="https://github.com/roundcube/roundcubemail/releases/download/${VERSION}/roundcubemail-${VERSION}.tar.gz"
