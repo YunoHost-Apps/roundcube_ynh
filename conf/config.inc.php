97668c2e7f4b035f96b7c59076c144b58def20b6
ericgaspar <junk.eg@free.fr>
2020-11-09 16:50:06 +0100

    Revert "Add domain"
    This reverts commit 155698f1476e8d04079eef06de68a0f5474414ae.

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -44,7 +44,7 @@ $config['db_dsnw'] = 'mysql://__DBUSER__:__DBPASS__@localhost/__DBNAME__';
 // %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
 // %s - domain name after the '@' from e-mail address provided at login screen
 // For example %n = mail.domain.tld, %t = domain.tld
-$config['default_host'] = '__DOMAIN__';
+$config['default_host'] = 'localhost';
 
 // SMTP server host (for sending mails).
 // To use SSL/TLS connection, enter hostname with prefix ssl:// or tls://
