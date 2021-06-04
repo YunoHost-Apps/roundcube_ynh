727639deb95b6b9a2b069d7a75af87c6c76d9aa6
ericgaspar <junk.eg@free.fr>
2021-06-04 16:09:37 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,7 +10,7 @@ YNH_PHP_VERSION="7.3"
 extra_php_dependencies="php-pear php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-curl"
 
 # Composer version
-YNH_COMPOSER_VERSION=2.0.11
+YNH_COMPOSER_VERSION=2.1.1
 
 # Plugins version
 contextmenu_version=2.3
