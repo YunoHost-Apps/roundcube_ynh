bb7c8f3ca55aa91f62632ab7389affef48bd4d5a
ericgaspar <junk.eg@free.fr>
2021-12-04 12:18:32 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,9 +13,9 @@ pkg_dependencies="php-pear php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysq
 YNH_COMPOSER_VERSION=2.1.1
 
 # Plugins version
-contextmenu_version=3.2.1
+contextmenu_version=3.3
 automatic_addressbook_version=v0.4.3
-carddav_version=3.0.3
+carddav_version=4.3.0
 
 #=================================================
 # EXPERIMENTAL HELPERS
