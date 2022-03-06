3c1cedef1e663311242e56e726fbb8a2f43c1fb1
ericgaspar <junk.eg@free.fr>
2022-03-06 22:55:23 +0100

    PHP8.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,13 +4,13 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="8.0"
 
 # Package dependencies
 pkg_dependencies="php-pear php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-curl"
 
 # Composer version
-YNH_COMPOSER_VERSION=2.1.1
+YNH_COMPOSER_VERSION=2.2.7
 
 # Plugins version
 contextmenu_version=3.3
