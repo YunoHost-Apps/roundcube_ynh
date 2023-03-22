bb61118a882a5d7ac76658cb13ca6fb5902e1b39
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-22 07:47:45 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,14 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="8.0"
-
-# Package dependencies
-#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-curl"
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="php-pear $php_dependencies"
-
 # Composer version
 YNH_COMPOSER_VERSION=2.3.7
 
