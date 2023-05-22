71deb5d809eef97c864aabbadfab64b277665a45
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-22 12:00:26 +0200

    Testing (#170)
    * Auto-update README
    
    * Update _common.sh (#166)
    
    * Update _common.sh
    
    * Update install
    
    * Update upgrade
    
    * fix
    
    * Update manifest.json
    
    * Auto-update README
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,7 +15,7 @@ YNH_COMPOSER_VERSION=2.3.7
 # Plugins version
 contextmenu_version=3.3.1
 automatic_addressbook_version=v0.4.3
-carddav_version=4.4.3
+carddav_version=5.0.1
 
 #=================================================
 # EXPERIMENTAL HELPERS
