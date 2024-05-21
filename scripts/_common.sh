a399599a8994a867664927c02a6b496d37441c7a
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-05-21 14:06:08 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,14 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# Composer version
-YNH_COMPOSER_VERSION=2.7.6
-
-# Plugins version
-contextmenu_version=3.3.1
-automatic_addressbook_version=v0.4.3
-carddav_version=5.1.0
-
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
