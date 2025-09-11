11db576d4ad1bd3881b6ba9e173e4393ea021c2a
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-09-11 13:55:45 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-# Composer version
-composer_version=2.8.5
-
 # Plugins version
 contextmenu_version=3.3.1
 automatic_addressbook_version=v0.4.3
