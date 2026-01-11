733148515cd1e86ed652b1a3f1370106acb0faf9
CodeShakingSheep <19874562+CodeShakingSheep@users.noreply.github.com>
2026-01-11 06:27:01 -0500

    Update carddav version to 5.1.2 (#248)

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,7 +10,7 @@ YNH_COMPOSER_VERSION=2.5.5
 # Plugins version
 contextmenu_version=3.3.1
 automatic_addressbook_version=v0.4.3
-carddav_version=5.1.0
+carddav_version=5.1.2
 
 #=================================================
 # EXPERIMENTAL HELPERS
