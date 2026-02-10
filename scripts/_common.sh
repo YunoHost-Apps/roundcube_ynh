d0e0d19a39a85db4fcbf49150fe141ac5b847bc8
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2026-02-10 21:59:18 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 php_version=8.3
 # Composer version
-YNH_COMPOSER_VERSION=2.5.5
+#YNH_COMPOSER_VERSION=2.5.5
 
 # Plugins version
 contextmenu_version=3.3.1
