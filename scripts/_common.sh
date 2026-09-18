cb98305a248402e34dc5a6d9c26035abdf1f2fb6
eric <46165813+ericgaspar@users.noreply.github.com>
2026-09-18 19:46:55 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,6 +5,9 @@
 #=================================================
 
 # Plugins version
+#https://plugins.roundcube.net/#/packages/johndoh/contextmenu
 contextmenu_version=3.3.1
+#https://plugins.roundcube.net/#/packages/projectmyst/automatic_addressbook
 automatic_addressbook_version=dev-master
+#https://plugins.roundcube.net/#/packages/roundcube/carddav
 carddav_version=5.1.3
